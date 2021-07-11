a9f089788dc3f42ab40c2bb6a403731b6c59d014
harisiva1 <harijegan19@gmail.com>
2021-07-11 12:45:26 +0200

    First commit

---
+++ build/my_gurdy_description/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/catkin_ws/src/my_gurdy_description
+# Build directory: /home/user/catkin_ws/build/my_gurdy_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
